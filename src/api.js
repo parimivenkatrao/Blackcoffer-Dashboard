3f0e7e5a31920c366dec508733de2477f8b5812b
PARIMI VENKATARAO <165083459+parimivenkatrao@users.noreply.github.com>
2025-12-02 11:41:24 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Vite exposes env vars prefixed with VITE_ to the client
-const baseURL = import.meta.env.VITE_API_BASE_URL || "https://backend-1-9gn3.onrender.com/api" //|| "http://localhost:5000/api";
+const baseURL = import.meta.env.VITE_API_BASE_URL || "https://backend-2-ntny.onrender.com/api" //|| "http://localhost:5000/api";
 
 export const api = axios.create({
   baseURL
